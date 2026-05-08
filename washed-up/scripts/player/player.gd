08bc9051f391467d98e27a465fb07fc8ac799796
R4nd0m <mathwizard8888@gmail.com>
2026-05-08 21:37:35 +1000

    Player moves

--- washed-up/scripts/player/player.gd
+++ washed-up/scripts/player/player.gd
@@ -1,11 +1,11 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide()

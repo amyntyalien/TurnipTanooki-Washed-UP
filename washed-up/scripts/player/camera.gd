38e0934dcf1ba986f9a127c25c2274ce835248ae
R4nd0m <mathwizard8888@gmail.com>
2026-05-09 09:31:44 +1000

    Camera doesn't show edge of map + game now in 1920x1080.

--- washed-up/scripts/player/camera.gd
+++ washed-up/scripts/player/camera.gd
@@ -1,10 +1,11 @@
-extends Node
+extends Camera2D
 
 @export var follow_speed := 5.0
 
 func _process(delta):
 	if get_parent() == null:
 		return
-	global_position = global_position_lerp(
+	# lerp should usually use the target position, not self.global_position
+	global_position = global_position.lerp(
 		get_parent().global_position, follow_speed * delta
 	)

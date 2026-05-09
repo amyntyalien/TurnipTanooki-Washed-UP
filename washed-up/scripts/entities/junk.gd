dbfcfca13c6e2b89d0b41282a3b0ad4297fe7c7b
A-A-wrong <aaronhuang0214@gmail.com>
2026-05-09 19:12:50 +1000

    AHHHHHHHHHHH

--- washed-up/scripts/entities/junk.gd
+++ washed-up/scripts/entities/junk.gd
@@ -1,36 +1,29 @@
 extends Node2D
 
+var trashes = []
 func _ready():
 	# gets Wood, Plastic, etc.
 	var all_visuals = get_children()
 	if all_visuals.is_empty():
 		push_error("No junk visuals found in junk.tscn!")
 		return
-
-	for child in all_visuals: # Hide everything first
-		if child is CanvasItem: # make sure it can be visible
-			child.visible = false
 	
 	var num_to_show = randi_range(1, min(3, all_visuals.size())) # How many (min 1)
 	
 	for i in range(num_to_show):
-		var random_choice = all_visuals.pick_random()
+		trashes.append(all_visuals.pick_random())
 		
-		# If already visible, try to pick another, or skip to avoid duplicates
-		if random_choice.visible:
-			# try again once
-			random_choice = all_visuals.pick_random()
-			
-		random_choice.visible = true
 		set_process_input(true)
 		
 # when junk is clicked on with the left mouse key, it goes to Inventory.add_trash().
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed():
-			_on_junk_clicked()
-			
-func _on_junk_clicked():
-	print("Junk clicked!")
-	Inventory.add_trash()
-	queue_free()
+
+func _process(_delta):
+	print(JSON.stringify(Inventory.trash))
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_C:
+			print("AAAA")
+			for i in range(randi_range(1, 10)):
+				Inventory.add_trash(["wood", "bottle", "plastic", "pizza"].pick_random(), randi_range(1,3))
+			set_process_input(false)

4ad7880d00db0227a08251b400cb6e70e6ba71d3
R4nd0m <mathwizard8888@gmail.com>
2026-05-09 11:05:51 +1000

    Cutscene has text with instructions.

--- washed-up/scripts/core/cutscene.gd
+++ washed-up/scripts/core/cutscene.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+@onready var label = $Label # the label child node
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	label.visible = false
+	
+	await get_tree().create_timer(5.0).timeout # waiting 5 seconds
+	
+	label.visible = true # label now visible
+	label.text = "Press F to skip the cutscene."
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

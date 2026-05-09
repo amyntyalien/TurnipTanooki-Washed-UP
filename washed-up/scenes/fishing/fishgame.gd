2779e7eb560cb55dccfbf9363e21dc624fa69b3d
A-A-wrong <aaronhuang0214@gmail.com>
2026-05-09 14:20:20 +1000

    Fishing game

--- washed-up/scenes/fishing/fishgame.gd
+++ washed-up/scenes/fishing/fishgame.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
+var combo = 0
+var timer = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_level1()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	timer+=1
+	if timer == 10:
+		combo = 0
+	timer = 0
+	pass
+
+func _level1():
+	var x = $Mathquestions
+	print(x.generate_question_level_1())
 	pass

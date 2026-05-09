49b60db48d159ba01ba9c81045d56cfa6ed38db8
R4nd0m <mathwizard8888@gmail.com>
2026-05-10 09:17:49 +1000

    Finally got this working again...

--- washed-up/scripts/core/cutscene.gd
+++ washed-up/scripts/core/cutscene.gd
@@ -9,7 +9,7 @@ var timer = 0
 func _ready():
 	label.visible = false
 	
-	await get_tree().create_timer(2.0).timeout # waiting 2 seconds
+	await get_tree().create_timer(1.5).timeout # waiting 2 seconds
 	
 	label.visible = true # label now visible
 	label.text = "Press F to skip the cutscene."
@@ -19,7 +19,7 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("skip_cutscene"):
 		get_tree().change_scene_to_file("res://scenes/core/gameworld.tscn")
-	if(timer == 150):
+	if(timer == 125):
 		timer = 0
 		frame += 1
 		if frame <= 6:

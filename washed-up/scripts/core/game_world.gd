9fb21739f8a7c8d3dfe34a35964b99151318705a
R4nd0m <mathwizard8888@gmail.com>
2026-05-09 11:52:43 +1000

    pause.

--- washed-up/scripts/core/game_world.gd
+++ washed-up/scripts/core/game_world.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().change_scene_to_file("res://scenes/core/pause.tscn")

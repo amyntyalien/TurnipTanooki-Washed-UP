84f3f6c750f31a6111f4530eba08468e0fe3719f
R4nd0m <mathwizard8888@gmail.com>
2026-05-09 10:43:49 +1000

    Concept Title Screen.

---
+++ washed-up/scripts/ui/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_new_game_pressed(): # New Game Texture Rect pressed
+	get_tree().change_scene_to_file("res://scenes/core/gameworld.tscn")
+
+func _on_continue_pressed():
+	print("No game to continue from!") # Placeholder for when save files get added
+	
+func _on_options_pressed():
+	print("No options for now!") # Another placeholder
+	
+func _on_quit_pressed():
+	get_tree().quit() # Should exit the game
+	

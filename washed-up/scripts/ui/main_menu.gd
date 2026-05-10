7606dba9ebc3e0c2755ad829b228a86e66683ea4
A-A-wrong <aaronhuang0214@gmail.com>
2026-05-10 11:23:51 +1000

    asdf

--- washed-up/scripts/ui/main_menu.gd
+++ washed-up/scripts/ui/main_menu.gd
@@ -5,9 +5,6 @@ func _on_new_game_pressed(): # New Game Texture Rect pressed
 	get_tree().change_scene_to_file("res://scenes/core/cutscene.tscn")
 
 func _on_continue_pressed():
-	if !SaveManager.has_save():
-		print("No save found!")
-		return
 	get_tree().change_scene_to_file("res://scenes/core/gameworld.tscn")
 	
 func _on_options_pressed():

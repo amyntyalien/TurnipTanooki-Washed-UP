37bd6d9a9c86365c22b67e904acb32622262e576
R4nd0m <mathwizard8888@gmail.com>
2026-05-10 10:38:15 +1000

    new titlescreen and pause menu

--- washed-up/scripts/core/cutscene.gd
+++ washed-up/scripts/core/cutscene.gd
@@ -18,12 +18,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("skip_cutscene"):
-		get_tree().change_scene_to_file("res://scenes/core/titlescreen.tscn")
+		get_tree().change_scene_to_file("res://scenes/core/gameworld.tscn")
 	if(timer == 125):
 		timer = 0
 		frame += 1
 		if frame <= 10:
 			$cutscene.play(str(frame))
 		else:
-			get_tree().change_scene_to_file("res://scenes/core/titlescreen.tscn")
+			get_tree().change_scene_to_file("res://scenes/core/gameworld.tscn")
 	timer+=1
